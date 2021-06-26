82b785660e58b8aabe845168bb8420844170789a
Blackduck <lhx11187@qq.com>
2021-06-26 13:56:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,24 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/sendmail', (req, res) => {
+  res.send({
+            "subject":"中文测试",
+            "body":"邮件测试",
+            "mailto":"lhx11187@qq.com"
+    })
+})
   .post('/sendmail', (req, res) => {
   //console.log(req.body);
-  res.set('000','success')
+  //res.set('000','success')
+  /*
     res.send({
         msg: 'set演示'
+    })*/
+      res.send({
+          "result": "Success",
+          "subject": "中文测试"
     })
+
 })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
